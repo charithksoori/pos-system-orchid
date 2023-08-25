0e894b0acde347c8aa5594783a40760103c6c972
charithksoori <116133799+charithksoori@users.noreply.github.com>
2023-08-25 17:53:28 +0530

    completed customer data process

--- src/com/orchid/pos/dao/DatabaseAccessCode.java
+++ src/com/orchid/pos/dao/DatabaseAccessCode.java
@@ -5,6 +5,7 @@ import com.orchid.pos.dto.UserDto;
 import com.orchid.pos.util.PasswordManager;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseAccessCode {
@@ -48,23 +49,70 @@ public class DatabaseAccessCode {
         return preparedStatement.executeUpdate()>0;
     }
 
-    public static boolean updateCustomer(String email, String name, String contact,double salary){
-        return false;
+    public static boolean updateCustomer(String email, String name, String contact,double salary) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_orchid","root","1241");
+        String sql = "UPDATE customer SET name=?, contact=? , salary=? WHERE email=?";
+        PreparedStatement preparedStatement= connection.prepareStatement(sql);
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, contact);
+        preparedStatement.setDouble(3, salary);
+        preparedStatement.setString(4,email);
+        return preparedStatement.executeUpdate()>0;
     }
 
-    public static CustomerDto findCustomer(String email){
+    public static CustomerDto findCustomer(String email) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_orchid","root","1241");
+        String sql = "SELECT * FROM customer WHERE email=?";
+        PreparedStatement preparedStatement= connection.prepareStatement(sql);
+        preparedStatement.setString(1,email);
+        ResultSet resultSet= preparedStatement.executeQuery();
+        if(resultSet.next()){
+            return new CustomerDto(resultSet.getString(1),resultSet.getString(2),resultSet.getString(3),resultSet.getDouble(4))
+        }
         return null;
     }
 
-    public static boolean deleteCustomer(String email){
-        return false;
+    public static boolean deleteCustomer(String email) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_orchid","root","1241");
+        String sql = "DELETE FROM customer WHERE email=?";
+        PreparedStatement preparedStatement= connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        return preparedStatement.executeUpdate()>0;
     }
 
-    public static List<CustomerDto> findAllCustomers(){
-        return null;
+    public static List<CustomerDto> findAllCustomers() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_orchid","root","1241");
+        String sql = "SELECT * FROM customer";
+        PreparedStatement preparedStatement= connection.prepareStatement(sql);
+        ResultSet resultSet= preparedStatement.executeQuery();
+
+        List<CustomerDto> dtos = new ArrayList<>()
+        while(resultSet.next()){
+            dtos.add( new CustomerDto(resultSet.getString(1),resultSet.getString(2),resultSet.getString(3),resultSet.getDouble(4)));
+        }
+        return dtos;
     }
-    public static List<CustomerDto> searchCustomers(String searchText){
-        return null;
+    public static List<CustomerDto> searchCustomers(String searchText) throws ClassNotFoundException, SQLException {
+
+        searchText = "%"+searchText+"%";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_orchid","root","1241");
+        String sql = "SELECT * FROM customer WHERE email LIKE  ? || name LIKE ?";
+        PreparedStatement preparedStatement= connection.prepareStatement(sql);
+        preparedStatement.setString(1,searchText);
+        preparedStatement.setString(2,searchText);
+        ResultSet resultSet= preparedStatement.executeQuery();
+
+        List<CustomerDto> dtos = new ArrayList<>()
+        while(resultSet.next()){
+            dtos.add( new CustomerDto(resultSet.getString(1),resultSet.getString(2),resultSet.getString(3),resultSet.getDouble(4)));
+        }
+        return dtos;
     }
 
 
